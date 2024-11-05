6744cedbef77686f27fa61b34498e85176cfa666
Sandhavi Wanigasooriya <sandhaviwanigasooriya@gmail.com>
2024-11-05 18:21:19 +0530

    Add authentication views for login and registration

---
+++ views/register.php
@@ -0,0 +1,36 @@
+<?php
+
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Register Page</title>
+
+</head>
+<body>
+
+<form action="" method="POST">
+    <label for="name">First Name:</label><br>
+    <input type="text" id="firstname" name="firstname" placeholder="Enter your first name"><br>
+
+    <label for="name">Last Name:</label><br>
+    <input type="text" id="lastname" name="lastname" placeholder="Enter your last name"><br>
+
+    <label for="email">Email:</label><br>
+    <input type="email" id="email" name="email" placeholder="Enter your email"><br>
+
+    <label for="password">Password:</label><br>
+    <input type="password" id="password" name="password" placeholder="Enter your password"><br>
+
+    <label for="password">Confirm Password:</label><br>
+    <input type="password" id="password" name="password" placeholder="Confirm your password"><br>
+
+    <button type="submit">Register</button>
+</form>
+
+</body>
+</html>
+
