e939adf0af250ffb8e5fc1661711485ae303e9ae
Sandhavi Wanigasooriya <sandhaviwanigasooriya@gmail.com>
2024-11-05 12:55:03 +0530

    Add login and register routes to the application

--- public/index.php
+++ public/index.php
@@ -14,5 +14,9 @@ $app -> router->get('/', [SiteController::class, 'home']);
 $app -> router->get('/contact', [SiteController::class, 'contact']);
 $app -> router->post('/contact', [SiteController::class, 'handleContact']);
 
+$app -> router->get('/login', [AuthController::class, 'login']);
+$app -> router->post('/login', [AuthController::class, 'login']);
+$app -> router->get('/register', [AuthController::class, 'register']);
+$app -> router->post('/register', [AuthController::class, 'register']);
 
 $app->run();
