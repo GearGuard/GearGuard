7f9aa5cda5031fd274082c44f91c8c8f5113ed52
Sandhavi Wanigasooriya <sandhaviwanigasooriya@gmail.com>
2024-11-05 18:58:45 +0530

    Add registration handling and validation in AuthController

--- controllers/AuthController.php
+++ controllers/AuthController.php
@@ -4,6 +4,7 @@ namespace app\controllers;
 
 use app\core\Controller;
 use app\core\Request;
+use app\models\RegisterModel;
 
 class AuthController extends Controller
 {
@@ -14,10 +15,20 @@ class AuthController extends Controller
     }
     public function register(Request $request)
     {
+        $errors = [];
         if($request->isPost()){
+            $registerModel = new RegisterModel();
+
+            $firstname = $request->getBody()['firstname'];
+            if(!$firstname){
+                $errors['firstname'] = 'First name is required';
+            }
+
             return 'Handling Submitted Data';
         }
         $this->setLayout('auth');
-        return $this->render('register');
+        return $this->render('register', [
+            'errors' => $errors
+        ]);
     }
 }
