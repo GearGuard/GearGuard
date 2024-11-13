9015befe5d67c17c1f4c24e1052155e3079bbcbe
Sandhavi Wanigasooriya <sandhaviwanigasooriya@gmail.com>
2024-11-13 12:33:11 +0530

    Implement Page titles

--- core/form/InputField.php
+++ core/form/InputField.php
@@ -3,8 +3,9 @@
 namespace app\core\form;
 
 use app\core\Model;
+use app\core\form\BaseField;
 
-class Field
+class Field extends BaseField
 {
 	public const TYPE_TEXT = 'text';
 	public const TYPE_PASSWORD = 'password';
@@ -21,34 +22,26 @@ class Field
 	public function __construct(\app\core\Model $model, string $attribute)
 	{
 		$this->type = self::TYPE_TEXT;
-		$this->model = $model;
-		$this->attribute = $attribute;
+		parent::__construct($model, $attribute);
 	}
-	public function __toString()
-	{
-		return sprintf(
-			'
-			<div class="form-group">
-        		<label class="form-label">%s</label>
-				<input type="%s" name="%s" value="%s" placeholder="Enter your %s" class="form-input%s">
-				<div class="invalid-feedback">
-					%s
-				</div>
-			</div>
-			',
-			$this->model->getLabel($this->attribute),
-			$this->type,
-			$this->attribute,
-			$this->model->{$this->attribute},
-			$this->model->getLabel($this->attribute),
-			$this->model->hasError($this->attribute) ? 'is-invalid' : '',
-			$this->model->getFirstError($this->attribute)
-		);
-	}
+
 
 	public function passwordField()
 	{
 		$this->type = self::TYPE_PASSWORD;
 		return $this;
+	}
+
+	public function renderInput(): string
+	{
+		return sprintf(
+			'
+			<input type="%s" name="%s" value="%s" placeholder="Enter your %s" class="form-input%s">',
+			$this->type,
+			$this->attribute,
+			$this->model->{$this->attribute},
+			$this->model->getLabel($this->attribute),
+			$this->model->hasError($this->attribute) ? 'is-invalid' : '',
+		);
 	}
 }
