2516fec9f33b1a10611a5ca81c10098f3ba15eb1
Pasindu Ravimal <34201061+PasinduRavimal@users.noreply.github.com>
2024-12-01 11:08:39 +0530

    Vehicle id is added to appointment

--- models/VehicleOwner.php
+++ models/VehicleOwner.php
@@ -7,21 +7,36 @@ use gearguard\phpmvc\Application;
 
 class VehicleOwner extends User
 {
-    private $vehicle_list;
+    private array $vehicle_list;
 
     public function __construct()
     {
         $this->updateVehicleList();
     }
 
+    public function tableName(): string
+    {
+        return "gg_user_owner";
+    }
+
+    public function primaryKeys(): array
+    {
+        return ['vehicle_id', 'user_id'];
+    }
+
     public function updateVehicleList()
     {
-        $sql = "SELECT * FROM gg_user_owner WHERE user_id = :user_id";
+        $sql = "SELECT * FROM gg_user_owner uo LEFT JOIN gg_vehicle v ON uo.vehicle_id = v.id WHERE uo.user_id = :user_id";
         $statement = Application::$app->db->prepare($sql);
         $statement->bindValue(':user_id', Application::$app->session->get('user'));
         $statement->execute();
 
         $this->vehicle_list = $statement->fetchAll();
+    }
+
+    public function getOwnedVehiclesList() : array
+    {
+        return $this->vehicle_list;
     }
 
 }
