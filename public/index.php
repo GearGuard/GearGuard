8d597b8c3c76fa5074b9f06fdd1412c785888a9e
Sandhavi Wanigasooriya <sandhaviwanigasooriya@gmail.com>
2025-04-23 18:57:27 +0530

    feat: add routes for viewing vehicle service history in ServicePerformController

--- public/index.php
+++ public/index.php
@@ -10,6 +10,7 @@ use app\controllers\GarageController;
 use app\controllers\SparePartController;
 use app\controllers\VehicleController;
 use app\controllers\AppointmentController;
+use app\controllers\ServicePerformController;
 use gearguard\phpmvc\Application;
 
 
@@ -63,6 +64,14 @@ $app->router->post('/customer/vehicle/register', [VehicleController::class, 'add
 $app->router->get('/customer/vehicle/my', [VehicleController::class, 'viewMyVehicleDetails']);
 $app->router->post('/customer/vehicle/update', [VehicleController::class, 'updateVehicle']);
 $app->router->post('/customer/vehicle/delete', [VehicleController::class, 'deleteVehicle']);
+
+
+//Vehicle/Service History
+$app->router->get('/customer/appointment/service_history', [ServicePerformController::class, 'viewServiceHistory']);
+$app->router->post('/customer/appointment/vehicle_service_history', [ServicePerformController::class, 'viewVehicleServiceHistory']);
+$app->router->get('/customer/appointment/get_vehicle_service_history', [ServicePerformController::class, 'getVehicleServiceHistory']);
+
+
 
 $app->router->get('/customer/vehicle/register', [AuthController::class, 'addVehicle']);
 
