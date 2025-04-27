ac2d241ecd42230841139ab55dc1722314f8efcf
Pasindu Ravimal <34201061+PasinduRavimal@users.noreply.github.com>
2025-04-27 13:29:35 +0530

    Bug fix

--- models/GarageService.php
+++ models/GarageService.php
@@ -2,6 +2,7 @@
 
 namespace app\models;
 
+use app\controllers\GarageController;
 use Couchbase\InvalidStateException;
 use gearguard\phpmvc\Application;
 use gearguard\phpmvc\db\DbModel;
@@ -106,7 +107,19 @@ class GarageService extends DbModel
         if (!$this->validate())
             throw new \Exception(array_values($this->errors)[0][0], 400);
 
-        return parent::save();
+        try {
+            return parent::save();
+        } catch (\PDOException $e) {
+            $this->id = GarageService::getServiceIDofServiceType($this->garage_id, $this->type);
+            return $this->update([
+                'type' => $this->type,
+                'price' => $this->price,
+                'duration' => $this->duration,
+                'description' => $this->description,
+                'garage_id' => $this->garage_id,
+                'status_id' => $this->status_id
+            ]);
+        }
     }
 
     public function rules(): array
@@ -188,12 +201,12 @@ class GarageService extends DbModel
 
     public static function verifyServiceExistance(int $garageID, int $serviceID) : bool
     {
-        $sql = "SELECT ggs.id FROM gearguard.gg_garage_service ggs WHERE ggs.garage_id = :garage_id AND ggs.id = :service_id";
+        $sql = "SELECT ggs.id FROM gearguard.gg_garage_service ggs WHERE ggs.garage_id = :garage_id AND ggs.id = :service_id LIMIT 1";
         $statement = Application::$app->db->prepare($sql);
         $statement->bindValue(':garage_id', $garageID, \PDO::PARAM_INT);
         $statement->bindValue(':service_id', $serviceID, \PDO::PARAM_INT);
         $statement->execute();
-        $result = $statement->fetchAll(\PDO::FETCH_ASSOC);
+        $result = $statement->fetch(\PDO::FETCH_ASSOC);
         if (count($result) > 0)
             return true;
 
@@ -202,16 +215,30 @@ class GarageService extends DbModel
 
     public static function verifyServiceTypeExistance(int $garageID, string $service_type) : bool
     {
-        $sql = "SELECT ggs.id FROM gearguard.gg_garage_service ggs WHERE ggs.garage_id = :garage_id AND ggs.type = :service_type";
+        $sql = "SELECT ggs.id FROM gearguard.gg_garage_service ggs WHERE ggs.garage_id = :garage_id AND ggs.type = :service_type AND ggs.status_id = 2 LIMIT 1";
         $statement = Application::$app->db->prepare($sql);
         $statement->bindValue(':garage_id', $garageID, \PDO::PARAM_INT);
-        $statement->bindValue(':service_type', $service_type, \PDO::PARAM_INT);
+        $statement->bindValue(':service_type', $service_type);
         $statement->execute();
         $result = $statement->fetchAll(\PDO::FETCH_ASSOC);
         if (count($result) > 0)
             return true;
 
         return false;
+    }
+
+    public static function getServiceIDofServiceType(int $garageID, string $service_type) {
+        $sql = "SELECT ggs.id FROM gearguard.gg_garage_service ggs WHERE ggs.garage_id = :garage_id AND ggs.type = :service_type LIMIT 1";
+        $statement = Application::$app->db->prepare($sql);
+        $statement->bindValue(':garage_id', $garageID, \PDO::PARAM_INT);
+        $statement->bindValue(':service_type', $service_type);
+        $statement->execute();
+        $result = $statement->fetch(\PDO::FETCH_ASSOC);
+
+        if ($result)
+            return $result['id'];
+
+        return null;
     }
 
     public static function getGarageOfService(int $serviceID)
